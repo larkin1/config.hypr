b8adb8f765eff28122571f83c94be19450504633
larkin <88820972+larkin1@users.noreply.github.com>
2026-05-12 17:29:31 +1000

    started adding lua config files

---
+++ hyprland/monitors.lua
@@ -0,0 +1,22 @@
+hl.monitor({
+  output = "DP-1",
+  mode = "highrr",
+  position = "0x300",
+  scale = "1",
+})
+
+hl.monitor({
+  output = "DP-2",
+  mode = "highrr",
+  position = "2560x0",
+  scale = "1",
+  transform = 1,
+})
+
+hl.monitor({
+  output = "DP-3",
+  mode = "highrr",
+  position = "-1920x0",
+  scale = "1",
+  transform = 3,
+})

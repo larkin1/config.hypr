9fa4d050d24be68a2d737ef666b8e60567ebf303
Larkin <88820972+larkin1@users.noreply.github.com>
2026-05-14 14:41:17 +1000

    i committed from my work pc and now everything is messed up ahhh

--- lua/monitors.lua
+++ lua/monitors.lua
@@ -1,22 +1,22 @@
 hl.monitor({
   output = "DP-1",
   mode = "highrr",
-  position = "0x430",
+  position = "0x300",
   scale = "1",
 })
 
--- hl.monitor({
---   output = "DP-2",
---   mode = "highrr",
---   position = "2560x0",
---   scale = "1",
---   transform = 1,
--- })
+hl.monitor({
+  output = "DP-2",
+  mode = "highrr",
+  position = "2560x0",
+  scale = "1",
+  transform = 1,
+})
 
 hl.monitor({
   output = "DP-3",
   mode = "highrr",
-  position = "1920x0",
+  position = "-1080x0",
   scale = "1",
   transform = 3,
 })

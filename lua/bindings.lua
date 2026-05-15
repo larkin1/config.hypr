ebdb19b8d9a4f3789f8a2610ca05fffb40e4d3bb
Larkin <88820972+larkin1@users.noreply.github.com>
2026-05-15 15:20:37 +1000

    i'm gonna get "bashed" for this HAHHAHAHHAH (no but seriously, why wouldn't you just add a script for that bro)

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -6,7 +6,9 @@ hl.bind(M.mainMod .. " + E", hl.dsp.exec_cmd(M.fileManager))
 hl.bind(M.mainMod .. " + R", hl.dsp.exec_cmd(M.runMenu))
 
 -- clipboard
-hl.bind(M.mainMod .. " + S", hl.dsp.exec_cmd("grim -g \"$(slurp -d)\" - | wl-copy -t image/png"))
+-- hl.bind(M.mainMod .. " + S", hl.dsp.exec_cmd("grim -g \"$(slurp -d)\" - | wl-copy -t image/png"))
+local cmd = [=[grim -g "$(slurp)" - | wl-copy -t image/png && action=$(dunstify "Screenshot taken" "Click to edit" -t 10000 --action="edit,Edit") && [[ "$action" == "edit" ]] && wl-paste | satty --filename - --output-filename "$HOME/Pictures/Screenshots/$(date '+%Y%m%d-%H%M%S').png"]=]
+hl.bind(M.mainMod .. " + S", hl.dsp.exec_cmd(cmd))
 hl.bind(M.mainMod .. " + V", hl.dsp.exec_cmd("cliphist list | rofi -dmenu -display-columns 2 | cliphist decode | wl-copy"))
 
 -- window commands

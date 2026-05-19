174dbfc25e99d0c3daf0b222b2942416b8e060e7
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-05-19 17:25:21 +1000

    fixing hypridle

--- lua/startup.lua
+++ lua/startup.lua
@@ -1,5 +1,13 @@
 hl.on("hyprland.start", function ()
-  hl.exec_cmd("hyprpaper & waybar & hypridle")
-  hl.exec_cmd("wl-paste --watch cliphist store")
-  hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
+  local cmds = {
+    "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland HYPRLAND_INSTANCE_SIGNATURE",
+    "hyprpaper",
+    "waybar",
+    "hypridle",
+    "wl-paste --watch cliphist store",
+    "systemctl --user start hyprpolkitagent.service",
+  }
+  for _, cmd in ipairs(cmds) do
+    hl.exec_cmd(cmd)
+  end
 end)

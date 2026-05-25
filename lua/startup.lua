06a248e1c68a9964da65cfd56181301a495aa571
larkin <88820972+larkin1@users.noreply.github.com>
2026-05-25 12:07:20 +1000

    added startup for custom waybar color cycler. waybar can be found at github.com/larkin1/config.waybar

--- lua/startup.lua
+++ lua/startup.lua
@@ -2,7 +2,7 @@ hl.on("hyprland.start", function ()
   local cmds = {
     "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland HYPRLAND_INSTANCE_SIGNATURE",
     "hyprpaper",
-    "waybar",
+    "~/.config/waybar/scripts/rgb-cycle.sh & waybar",
     "hypridle",
     "wl-paste --watch cliphist store",
     "systemctl --user start hyprpolkitagent.service",

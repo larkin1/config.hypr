29985311fd29c9a60e7c9f3aa0571b101256542b
larkin <88820972+larkin1@users.noreply.github.com>
2026-05-29 09:23:51 +1000

    changed monitor layout to a normal default layout.

--- lua/monitors.lua
+++ lua/monitors.lua
@@ -1,22 +1,6 @@
 hl.monitor({
-  output = "DP-1",
+  output = "",
   mode = "highrr",
-  position = "0x300",
+  position = "auto",
   scale = "1",
-})
-
-hl.monitor({
-  output = "DP-2",
-  mode = "highrr",
-  position = "2560x0",
-  scale = "1",
-  transform = 1,
-})
-
-hl.monitor({
-  output = "DP-3",
-  mode = "highrr",
-  position = "-1080x0",
-  scale = "1",
-  transform = 3,
 })

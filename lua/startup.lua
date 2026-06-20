fe2afd87bc7ce1cafb4774f5f83338391d7bfe52
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-06-20 21:03:33 +1000

    removed rebase stuff because idk how to use git

--- lua/startup.lua
+++ lua/startup.lua
@@ -2,14 +2,8 @@ hl.on("hyprland.start", function ()
   local cmds = {
     "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland HYPRLAND_INSTANCE_SIGNATURE",
     "hyprpaper",
-    -- "~/.config/waybar/scripts/rgb-cycle.sh & waybar",
-<<<<<<< HEAD
-      "waybar",
-      "hypridle",
-=======
     "waybar"
     "hypridle",
->>>>>>> 58088c1 (stop the rgb cycling on waybar)
     "wl-paste --watch cliphist store",
     "systemctl --user start hyprpolkitagent.service",
   }

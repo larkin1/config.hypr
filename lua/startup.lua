76643e1ca0b53b9bc436fe545ab962ebdefd65a3
larkin1 <88820972+larkin1@users.noreply.github.com>
2026-06-20 16:54:08 +1000

    stop the rgb cycling on waybar

--- lua/startup.lua
+++ lua/startup.lua
@@ -3,8 +3,13 @@ hl.on("hyprland.start", function ()
     "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland HYPRLAND_INSTANCE_SIGNATURE",
     "hyprpaper",
     -- "~/.config/waybar/scripts/rgb-cycle.sh & waybar",
+<<<<<<< HEAD
       "waybar",
       "hypridle",
+=======
+    "waybar"
+    "hypridle",
+>>>>>>> 58088c1 (stop the rgb cycling on waybar)
     "wl-paste --watch cliphist store",
     "systemctl --user start hyprpolkitagent.service",
   }
